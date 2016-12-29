f02435f16bdf263443ac590d1a075b1317c38225
Lars Greiss <kufikugel@googlemail.com>
2016-12-29 05:28:10 -0500

    Base: Navigation bar options (1/2) -Toggle on/off -Height and width
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4207,6 +4207,30 @@ public final class Settings {
         public static final String SCREEN_STATE_TWOG = "screen_state_twog";
 
         /**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
+         * Navigation bar height when it is on landscape
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
+
+        /**
+         * Navigation bar height when it is on landscape at the right
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+
+        /**
+         * Wether navigation bar is enabled or not
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
+        /**
          * @hide
          */
         public static final String SCREEN_STATE_GPS = "screen_state_gps";
