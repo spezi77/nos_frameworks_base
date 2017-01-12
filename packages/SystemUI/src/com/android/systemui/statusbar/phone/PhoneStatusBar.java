334b4a4de54c32f5ffcddb6cface135208e94e90
spezi77 <spezi77@gmx.com>
2017-01-12 21:49:01 +0100

    Revert "DUI: set bar IME hints when we add/change bar"
    This reverts commit 415a6af842c3ef45c341891023c81471f0c73af6.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1382,7 +1382,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mNavigationController.getBar().getBaseView().setVisibility(View.GONE);
         }
         mNavigationController.getBar().notifyInflateFromUser();
-        mNavigationController.getBar().setNavigationIconHints(mNavigationIconHints);
         addNavigationBar();
     }
 
